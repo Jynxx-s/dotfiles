9bf08697f95085bed0bfc616ea4f3947c2f14073
jacob <jb05.jacob@gmail.com>
2025-09-03 15:36:56 +1000

    fixed the formater issues

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,96 +1,96 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup({
-				ensure_installed = {},
-				"stylua", -- Lua formatter
-				"prettier", -- Prettier formatter
-				"black",
-				"isort",
-			})
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "pyright", "html", "cssls", "ts_ls",  "clangd", "cmake"},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			vim.lsp.config("lua_ls", {
-				capabilities = capabilities,
-				settings = {
-					["lua_ls"] = {},
-				},
-			})
-			vim.lsp.config("pyright", {
-				capabilities = capabilities,
-				settings = {
-					["pyright"] = {},
-				},
-			})
-			vim.lsp.config("html", {
-				capabilities = capabilities,
-				settings = {
-					["html"] = {},
-				},
-			})
-			vim.lsp.config("cssls", {
-				capabilities = capabilities,
-				settings = {
-					["cssls"] = {},
-				},
-			})
-			vim.lsp.config("ts_ls", {
-				capabilities = capabilities,
-				settings = {
-					["ts_ls"] = {},
-				},
-			})
-			vim.lsp.config("clangd", {
-				capabilities = capabilities,
-				cmd = {
-					"clangd",
-					"--compile-commands-dir=build", -- <-- add this line with your build folder path
-				},
-				settings = {
-					["clangd"] = {},
-				},
-			})
-			vim.lsp.config("cmake", {
-				capabilities = capabilities,
-				settings = {
-					["cmake"] = {},
-				},
-			})
-			vim.diagnostic.config({
-				virtual_text = {
-					prefix = "■", -- Could be '■', '▎', 'x'
-					spacing = 2,
-					source = "if_many",
-				},
-				signs = true,
-				underline = true,
-				update_in_insert = false,
-				severity_sort = true,
-				float = {
-					source = "always",
-					border = "rounded",
-					header = "",
-					prefix = "",
-				},
-			})
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup({
+                ensure_installed = {},
+                "stylua", -- Lua formatter
+                "prettier", -- Prettier formatter
+                "black",
+                "isort",
+            })
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls", "pyright", "html", "cssls", "ts_ls", "clangd", "cmake" },
+            })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        lazy = false,
+        config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            vim.lsp.config("lua_ls", {
+                capabilities = capabilities,
+                settings = {
+                    ["lua_ls"] = {},
+                },
+            })
+            vim.lsp.config("pyright", {
+                capabilities = capabilities,
+                settings = {
+                    ["pyright"] = {},
+                },
+            })
+            vim.lsp.config("html", {
+                capabilities = capabilities,
+                settings = {
+                    ["html"] = {},
+                },
+            })
+            vim.lsp.config("cssls", {
+                capabilities = capabilities,
+                settings = {
+                    ["cssls"] = {},
+                },
+            })
+            vim.lsp.config("ts_ls", {
+                capabilities = capabilities,
+                settings = {
+                    ["ts_ls"] = {},
+                },
+            })
+            vim.lsp.config("clangd", {
+                capabilities = capabilities,
+                cmd = {
+                    "clangd",
+                    "--compile-commands-dir=build", -- <-- add this line with your build folder path
+                },
+                settings = {
+                    ["clangd"] = {},
+                },
+            })
+            vim.lsp.config("cmake", {
+                capabilities = capabilities,
+                settings = {
+                    ["cmake"] = {},
+                },
+            })
+            vim.diagnostic.config({
+                virtual_text = {
+                    prefix = "■", -- Could be '■', '▎', 'x'
+                    spacing = 2,
+                    source = "if_many",
+                },
+                signs = true,
+                underline = true,
+                update_in_insert = false,
+                severity_sort = true,
+                float = {
+                    source = "always",
+                    border = "rounded",
+                    header = "",
+                    prefix = "",
+                },
+            })
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+        end,
+    },
 }
--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -2,14 +2,23 @@ return {
 	"nvimtools/none-ls.nvim",
 	config = function()
 		local null_ls = require("null-ls")
+
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettier,
 				null_ls.builtins.formatting.isort,
 				null_ls.builtins.formatting.black,
+
+				null_ls.builtins.formatting.clang_format.with({
+					extra_args = { "--style={IndentWidth: 4, TabWidth: 4, UseTab: Never}" },
+				}),
 			},
 		})
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+
+		vim.keymap.set("n", "<leader>gf", function()
+			vim.lsp.buf.format({ async = true })
+		end, {})
 	end,
 }
+
